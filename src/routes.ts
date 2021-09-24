ebee5b4dc5792647cb15be024a0aa8aac35b7bee
Maycon Fabio <m.vinnicyus@gmail.com>
2021-09-24 16:14:38 -0300

    Refactor files organization

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,7 @@
 import express from 'express'
-import puppeteer from 'puppeteer'
+import getPage from './controllers/getAnimes'
 
 const routes = express.Router()
-
-const getPage = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://animesonline.org/episodio/')
-  const html = await page.$$eval('article.item.se.episodes div.data', divs => {
-    return divs.map(div => {
-      return {
-        title: div.querySelector('span.serie')?.innerHTML,
-        episode: div.querySelector('h3 a')?.innerHTML,
-      }
-    })
-  })
-
-  
-  await browser.close()
-  return html
-}
 
 routes.get('/', async (req, res) => {
   const html = await getPage()
