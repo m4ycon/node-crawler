e4185a28f55c6fb1dc1be73efa535b89e8aa81bd
Maycon Fabio <m.vinnicyus@gmail.com>
2021-09-23 23:53:47 -0300

    Get a page and search for info

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,29 @@
 import express from 'express'
+import puppeteer from 'puppeteer'
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  res.send('Hello World!')
+const getPage = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://animesonline.org/episodio/')
+  const html = await page.$$eval('article.item.se.episodes div.data', divs => {
+    return divs.map(div => {
+      return {
+        title: div.querySelector('span.serie')?.innerHTML,
+        episode: div.querySelector('h3 a')?.innerHTML,
+      }
+    })
+  })
+
+  
+  await browser.close()
+  return html
+}
+
+routes.get('/', async (req, res) => {
+  const html = await getPage()
+  res.send(html)
 })
 
 export default routes
