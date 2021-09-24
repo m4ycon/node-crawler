e94766e1ec43f63e4cf226a5d9d3358610004f37
Maycon Fabio <m.vinnicyus@gmail.com>
2021-09-24 17:07:44 -0300

    Shows simple anime list

--- src/controllers/getAnimes.ts
+++ src/controllers/getAnimes.ts
@@ -1,11 +1,13 @@
 import puppeteer from 'puppeteer'
+import path from 'path'
+import fs from 'fs'
+import { parse } from 'node-html-parser'
 
-
-const getPage = async () => {
+const getAnimes = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('https://animesonline.org/episodio/')
-  const html = await page.$$eval('article.item.se.episodes div.data', divs => {
+  const animes = await page.$$eval('article.item.se.episodes div.data', divs => {
     return divs.map(div => {
       return {
         title: div.querySelector('span.serie')?.innerHTML,
@@ -13,10 +15,16 @@ const getPage = async () => {
       }
     })
   })
-
-  
   await browser.close()
+
+  const filepath = path.resolve(__dirname, '..', 'views', 'index.html')
+  const html = parse(fs.readFileSync(filepath, 'utf8'))
+
+  const animesP = animes.map(anime => parse(`<li>${anime.title} - ${anime.episode}</li>`))
+
+  animesP.forEach(anime => html.querySelector('ul').appendChild(anime))
+
   return html
 }
 
-export default getPage
+export default getAnimes
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
-import getPage from './controllers/getAnimes'
+import path from 'path'
+import getAnimes from './controllers/getAnimes'
 
 const routes = express.Router()
 
 routes.get('/', async (req, res) => {
-  const html = await getPage()
-  res.send(html)
+  const html = await getAnimes()
+  res.send(html.toString())
 })
 
 export default routes
