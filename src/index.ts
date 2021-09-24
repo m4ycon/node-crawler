126bcc7997f0b493472281fed34a968f9c7b92c8
Maycon Fabio <m.vinnicyus@gmail.com>
2021-09-23 23:04:54 -0300

    Add cors and separate routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+import routes from './routes'
 
 const app = express()
 const port = 7777
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
